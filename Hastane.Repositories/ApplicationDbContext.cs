7bb911f912d95574960ed927dfb7e689b429e101
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2023-07-27 13:04:34 +0300

    Add project files.

---
+++ Hastane.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Hastane.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hastane.Repositories
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
