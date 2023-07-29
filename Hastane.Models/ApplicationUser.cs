c654e9e33d6847780b2c1a36077242bc974db33e
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2023-07-29 14:27:15 +0300

    Models to DataContext,a new viewmodel named HospitalInfo,Paging,Service Model for HospitalInfo

--- Hastane.Models/ApplicationUser.cs
+++ Hastane.Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,9 @@ namespace Hastane.Models
         public DateTime DOB { get; set; }//dogum tarihi (Date Of Birth)
         public string Specialist { get; set; }
         public Department Department { get; set; }
+        [NotMapped]
         public ICollection<Appointment> Appointments { get; set; }
+        [NotMapped]
         public ICollection<Payroll> Payrolls { get; set; }
 
     }
@@ -26,6 +29,6 @@ namespace Hastane.Models
 {
     public enum Gender
     {
-        Male,Female
+        Male, Female
     }
 }
---
+++ Hastane.Services/IHospitalInfo.cs
@@ -0,0 +1,19 @@
+﻿using Hastane.Utilities;
+using Hastane.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hastane.Services
+{
+    public interface IHospitalInfo
+    {
+        PagedResult<HospitalInfoViewModel> GetAll(int pageNumber, int pageSize);
+        HospitalInfoViewModel GetHospitalById(int HospitalId);
+        void UpdateHospitalInfo(HospitalInfoViewModel HospitalInfo);
+        void InsertHospitalInfo(HospitalInfoViewModel hospitalInfo);
+        void DeleteHospitalInfo(int id);
+    }
+}
